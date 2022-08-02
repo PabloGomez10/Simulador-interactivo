fb96e69635c0657bb222ba4a11f373f7d3e3df02
PabloGomez10 <102038179+PabloGomez10@users.noreply.github.com>
2022-08-01 21:32:47 -0300

    a

---
+++ js/index.js
@@ -0,0 +1,39 @@
+let Bienvenido = prompt("ingresar tu nombre");
+
+if (Bienvenido == "") {
+
+    alert("Ingresa tu nombre " + Bienvenido);
+    Bienvenido = prompt("ingresar otro nombre");
+}
+
+let Apellido = prompt("ingresar tu apellido");
+
+if (Apellido == "") {
+
+    alert("Ingresa tu apellido " + Apellido);
+    Apellido = prompt("ingresar otro apellido");
+}
+
+let Usuario = prompt("ingresar tu Usuario");
+
+if (Usuario == "") {
+
+    alert("Ingresa tu usuario " + Usuario);
+    Usuario = prompt("ingresar otro usuario");
+}
+
+let Correo = prompt("ingresar tu correo");
+
+if (Correo == "") {
+
+    alert("Ingresa tu correo " + Correo);
+    Correo = prompt("ingresar otro correo");
+}
+
+let Contraseña = prompt("ingresar tu contraseña");
+
+if (Contraseña == "") {
+
+    alert("Ingresa tu contrasña " + Contraseña);
+    Contraseña = prompt("ingresar otra contraseña");
+}
